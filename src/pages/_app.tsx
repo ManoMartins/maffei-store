5faa5c2655144294af7b9037b07184957cd7f89b
Manoel Martins <mano.martins29@gmail.com>
2022-04-24 21:55:04 -0300

    :sparkles: setup styled components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import theme from '../global/styles/theme'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from './components/UI/atoms/Button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button />
+    </div>
+  )
+}
