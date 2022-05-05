9bd822055082b8a7a8ffbb2238ab0047a467ae92
Manoel Martins <mano.martins29@gmail.com>
2022-05-05 00:12:54 -0300

    :sparkles: add material ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,10 @@
-import { CustomProvider } from 'rsuite'
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from '../global/styles/global'
-import theme from '../global/styles/theme'
-
-import 'rsuite/dist/rsuite.min.css'; 
 import { CartContextProvider } from '../contexts/cart';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <CustomProvider theme='dark'>
-      <ThemeProvider theme={theme}>
-        <CartContextProvider>
-          <Component {...pageProps} />
-
-          <GlobalStyle />
-        </CartContextProvider>
-      </ThemeProvider>
-    </CustomProvider>
+    <CartContextProvider>
+      <Component {...pageProps} />
+    </CartContextProvider>
   )
 }
 
