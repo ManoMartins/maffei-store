0dd574b2f0a2134509a6f2e835ef63f9104462c8
Manoel Martins <mano.martins29@gmail.com>
2022-05-14 13:57:16 -0300

    :sparkles: add game skeleton

---
+++ src/components/UI/templates/Games/GameSkeleton/index.tsx
@@ -0,0 +1,32 @@
+import { Skeleton } from '@chakra-ui/react';
+
+export default function GameSkeleton() {
+  return (
+    <>
+      <Skeleton
+        maxWidth="217px"
+        minWidth="217px"
+        height="207px"
+        borderRadius="2"
+      />
+      <Skeleton
+        maxWidth="217px"
+        minWidth="217px"
+        height="207px"
+        borderRadius="2"
+      />
+      <Skeleton
+        maxWidth="217px"
+        minWidth="217px"
+        height="207px"
+        borderRadius="2"
+      />
+      <Skeleton
+        maxWidth="217px"
+        minWidth="217px"
+        height="207px"
+        borderRadius="2"
+      />
+    </>
+  );
+}
